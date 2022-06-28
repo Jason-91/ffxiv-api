b1587083d0e08d6b7be270c5c6f95e2d21a86e14
davchung5@gmail.com <david@Davids-MBP-2.lan>
2022-06-27 22:18:02 -0700

    search-itemname endpoint is implemented

--- index.js
+++ index.js
@@ -3,37 +3,67 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+// const xivApiPrivateKey = 'API_KEY';
 const xivApiPrivateKey = '77e71ea12ea143c7b0520295eb1cc31b0be74519e5924792917a4c191ae44fbc';
+// world IDs
 const worldIds = [34, 37, 41, 62, 74, 75, 81, 91];
-// GET search call
-app.get('/search', (req, res) => {
+// GET search item by item name
+/**
+ * use req.query.item_name to call xiv API to pull the item IDs[]
+ * use item IDs from xiv API to call universalis API and map item_id, item_name and item_type
+ */
+app.get('/search-itemname', (req, res) => {
     const item_name = req.query.item_name;
-
-    const xivApiQuery = `https://xivapi.com/search?string=${item_name}&indexes=Item?private_keys=${xivApiPrivateKey}`;
-    // const { item_name } = req.body;
-    
-    axiox.get(xivApi)
+    const xivApiGetItemDetailQuery = `https://xivapi.com/search?indexes=item&string=${item_name}&private_keys=${xivApiPrivateKey}`;
+    // `https://xivapi.com/search
+    // ?indexes=item
+    // &filters=ItemSearchCategory.ID%3E%3D1
+    // &columns=ID
+    // %2CIcon
+    // %2CName
+    // %2CLevelItem
+    // %2CRarity
+    // %2CItemSearchCategory.Name
+    // %2CItemSearchCategory.ID
+    // %2CItemKind.Name
+    // &string=calf
+    // &limit=100
+    // &sort_field=LevelItem&sort_order=desc
+    // &string_algo=fuzzy&language=en`
+    let result;
+    axios({
+        method: 'get',
+        url: xivApiGetItemDetailQuery
+    })
         .then((res) => {
-            const { Results } = res;
-            const listOfItemIds
-            // call xivapi.com with the itemname        
+            console.log('RESULTS', res)
+            const { data } = res;
+            const { Results } = data;
+            console.log(Results)
+            // const listOfItemIds
+            // call xivapi.com with the itemname
+            result = {
+                message: 'success',
+                data: Results.map(data => {
+                    return {   
+                        item_id: data.ID,
+                        item_name: data.Name,
+                        item_url: data.Url,
+                        item_icon: data.Icon,
+                    }
+                })
+            }        
 
         })
         .catch((err) => {
-
+            result = 'error: something happend to the ffxivapi';
         });
-        // if a successful array returns
-            // call the last updated
-    // axios.get("https://universalis.app/api/v2/data-centers")
-    // .then((res) => {
-    //     console.log(res.data)
-    // })
-    // .catch((err) => {
-    //     console.log(err)
-    // })
-    // ;
-    // res.send('Hello World!')
+    return result;
 });
+
+app.get('/item-market', (req, res) => {
+
+})
 
 app.listen(port, () => {
     console.log(`Server started in PORT: ${port}`)
