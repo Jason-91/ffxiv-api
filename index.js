e29f6efb3977ea74b0a5bba0db0cbf2fc26b2f4b
davchung5@gmail.com <david@Davids-MBP-2.lan>
2022-07-01 15:42:04 -0700

    /search-itemname implemented

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ const worldIds = [34, 37, 41, 62, 74, 75, 81, 91];
  * use req.query.item_name to call xiv API to pull the item IDs[]
  * use item IDs from xiv API to call universalis API and map item_id, item_name and item_type
  */
-app.get('/search-itemname', (req, res) => {
+app.get('/search-itemname', async (req, res) => {
+    console.log('called!')
     const item_name = req.query.item_name;
-    const xivApiGetItemDetailQuery = `https://xivapi.com/search?indexes=item&string=${item_name}&private_keys=${xivApiPrivateKey}`;
+    const xivApiGetItemDetailQuery = `https://xivapi.com/search?indexes=item&string=${item_name}&sort_field=LevelItem&sort_order=desc&limit=100&private_keys=${xivApiPrivateKey}`;
     // `https://xivapi.com/search
     // ?indexes=item
     // &filters=ItemSearchCategory.ID%3E%3D1
@@ -28,37 +29,38 @@ app.get('/search-itemname', (req, res) => {
     // %2CItemKind.Name
     // &string=calf
     // &limit=100
-    // &sort_field=LevelItem&sort_order=desc
+    // &sort_field=LevelItem
+    // &sort_order=desc
     // &string_algo=fuzzy&language=en`
     let result;
-    axios({
+    await axios({
         method: 'get',
         url: xivApiGetItemDetailQuery
     })
-        .then((res) => {
-            console.log('RESULTS', res)
-            const { data } = res;
-            const { Results } = data;
-            console.log(Results)
-            // const listOfItemIds
-            // call xivapi.com with the itemname
-            result = {
-                message: 'success',
-                data: Results.map(data => {
-                    return {   
-                        item_id: data.ID,
-                        item_name: data.Name,
-                        item_url: data.Url,
-                        item_icon: data.Icon,
-                    }
-                })
-            }        
+    .then((res) => { 
+        console.log('RESULTS', res)
+        const { data } = res;
+        const { Results } = data;
+        console.log(Results)
+        // const listOfItemIds
+        // call xivapi.com with the itemname
+        result = {
+            message: 'success',
+            data: Results.map(data => {
+                return {   
+                    item_id: data.ID,
+                    item_name: data.Name,
+                    item_url: data.Url,
+                    item_icon: data.Icon,
+                }
+            })
+        }        
 
-        })
-        .catch((err) => {
-            result = 'error: something happend to the ffxivapi';
-        });
-    return result;
+    })
+    .catch((err) => {
+        result = { message: 'error: something happend to the ffxivapi' };
+    });
+    res.send(result);
 });
 
 app.get('/item-market', (req, res) => {
